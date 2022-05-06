113e3f2ee695d1f300b6c2e38f03b9b955f4239a
Renan Oliveira <renan.sergipe68@gmail.com>
2022-05-05 22:03:07 -0300

    refactor: minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.listen(9090, () => console.log('server on'))
+//Método de chamada ao keycloak
+
+app.listen(9090, () => console.log('server on'));
